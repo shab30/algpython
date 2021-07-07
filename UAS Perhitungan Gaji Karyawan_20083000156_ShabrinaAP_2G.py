a11a70d5b958f162e585b9f9a35098b982a0c544
Shabrina Atha <85839998+shabrinaaputri@users.noreply.github.com>
2021-07-07 15:50:24 +0700

    Add files via upload

---
+++ UAS Perhitungan Gaji Karyawan_20083000156_ShabrinaAP_2G.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 15:23:17 2021
+
+@author: My Device is Awesome
+"""
+
+print("============================")
+print("Nama               = Haryono")
+print("Golongan           = 1")
+print("Status Kawin       = Kawin")
+print("Gaji Pokok         = 2500000")
+print("Tunjangan Istri    = 25000")
+print("Tunjangan Anak     = 50000")
+print(">>Gaji Bruto       = 2575000")
+print("============================")
+print("Biaya Jabatan      = 2587875")
+print("Iuran Pensiun      = 15500")
+print("Iuran Organisasi   = 3500")
+print(">>Gaji Netto       = 31875")
